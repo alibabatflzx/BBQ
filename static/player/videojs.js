a60f779767951b9513cd1676a31e10137b2f3b23
zoro <alibabatflzx@yahoo.com>
2019-03-22 18:14:52 +0800

    no message

--- static/player/videojs.js
+++ static/player/videojs.js
@@ -1,16 +1,28 @@
 /**
- * 判断是否是移动端
+ * 判断是否是IOS移动端
  */
-function getMobileBrowser(){
-	if(/Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent)){
-		return true;
-	}
+// function getMobileBrowser(){
+// 	if(/Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent)){
+// 		return true;
+// 	}
 //	else if(/Android/.test(navigator.userAgent) && MacPlayer.PlayUrl.indexOf(".m3u8")>0){
 //		return true;
 //	}
+// 	return false;
+// }
+
+/**
+ * 判断是否是移动端
+ */
+function getMobile(){
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+		return true;
+	}
 	return false;
 }
-if(getMobileBrowser()){
+
+
+if(getMobile()){
 	var pl = $("#playleft"),w = $(window).width(),h = w/16*9;		
 	pl.css({
 		width:w,
@@ -19,21 +31,9 @@ if(getMobileBrowser()){
 	document.write('<script type="text/javascript" src="'+ maccms.path +'/static/player/video.js"></script>');
 }else{
 	document.write('<script type="text/javascript" src="'+ maccms.path +'/static/player/ckplayer/ckplayer.js"></script>');
-
-	if(/Android/.test(navigator.userAgent)){	
-		var pl = $("#playleft"),w = $(window).width(),h = w/16*9;		
-		pl.css({
-			width:w,
-			height:h
-		});	
-		//getCkPlayerAdv();
-		$("#buffer").hide();
-		$("#install").hide();
-	}else{
-		MacPlayer.Html = '';
-		MacPlayer.Show();
-		loadCkPlayer();	
-	}
+	MacPlayer.Html = '';
+	MacPlayer.Show();
+	loadCkPlayer();
 }
 
 function loadCkPlayer(){
